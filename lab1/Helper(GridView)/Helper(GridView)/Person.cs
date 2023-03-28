97279839d470f015b2b5777de97317bdbfcc0558
raneem <raneem74>
2023-03-28 22:06:45 +0200

    lab1 added

---
+++ lab1/Helper(GridView)/Helper(GridView)/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helper_GridView_
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string gender { get; set; }
+    }
+}
